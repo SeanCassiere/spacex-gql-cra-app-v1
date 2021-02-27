21f723ed613082d66cabcca748b2a810474cb813
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-02-28 00:53:24 +0530

    Added Async-Await in Resolvers

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -4,17 +4,21 @@ const BASE_URL = "https://api.spacexdata.com/v3";
 
 const resolvers = {
 	Query: {
-		launches: () => {
-			return axios.get(`${BASE_URL}/launches`).then((res) => res.data);
+		launches: async () => {
+			const { data } = await axios.get(`${BASE_URL}/launches`);
+			return data;
 		},
-		launch: (root, { id }) => {
-			return axios.get(`${BASE_URL}/launches/${id}`).then((res) => res.data);
+		launch: async (_, { id }) => {
+			const { data } = await axios.get(`${BASE_URL}/launches/${id}`);
+			return data;
 		},
-		rockets: () => {
-			return axios.get(`${BASE_URL}/rockets`).then((res) => res.data);
+		rockets: async () => {
+			const { data } = await axios.get(`${BASE_URL}/rockets`);
+			return data;
 		},
-		rocket: (root, { id }) => {
-			return axios.get(`${BASE_URL}/rockets/${id}`).then((res) => res.data);
+		rocket: async (_, { id }) => {
+			const { data } = await axios.get(`${BASE_URL}/rockets/${id}`);
+			return data;
 		},
 	},
 };
